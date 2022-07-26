97195a34370b77638fa863635dcd323b0dc52909
Vivek Bose <waivek@gmail.com>
2022-07-26 18:17:15 +0530

    (auto-commit)

--- test.py
+++ test.py
@@ -214,7 +214,7 @@ def path_test():
 
 def ic_test():
     timer.start("import rich")
-    import rich
+    from rich import table
     timer.print("import rich")
     for i in range(1, 5):
         hundred = i * 20
