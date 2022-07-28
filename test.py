a8bcda3c3e59ca3a0fd3232d8ee882e809a137b0
Vivek Bose <waivek@gmail.com>
2022-07-28 14:47:30 +0530

    (auto-commit)

--- test.py
+++ test.py
@@ -420,11 +420,19 @@ def ic_test():
         # ic(L)
         # print()
 
-def profile_rich_imports():
-    # from rich.table import Table
-    # from rich.console import Console
-    # from rich import box
-    pass
+def tracefunc(frame, event, arg, indent=[0]):
+      if event == "call":
+          indent[0] += 2
+          print("-" * indent[0] + "> call function", frame.f_code.co_name)
+      elif event == "return":
+          print("<" + "-" * indent[0], "exit function", frame.f_code.co_name)
+          indent[0] -= 2
+      return tracefunc
+
+import sys
+sys.setprofile(tracefunc)
+
+main()   # or whatever kicks off your script
 
 
 def main():
