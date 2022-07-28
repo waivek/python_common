a3d6949761f37b99670020c87662651e6ad3cb5a
Vivek Bose <waivek@gmail.com>
2022-07-28 16:19:30 +0530

    (auto-commit)

--- test.py
+++ test.py
@@ -427,7 +427,7 @@ def tracefunc(frame, event, arg, indent=[0]):
     breakpoint()
     if event == "call":
         indent[0] += 2
-        print("-" * indent[0] + "> call function", frame.f_code.co_name)
+        print("-" * indent[0] + "> call function", frame.f_code.co_name, frame.f_code.co_filename)
     elif event == "return":
         print("<" + "-" * indent[0], "exit function", frame.f_code.co_name)
         indent[0] -= 2
