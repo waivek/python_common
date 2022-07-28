170b9d6fb151fd82bdec780ed9fffef5f4549308
Vivek Bose <waivek@gmail.com>
2022-07-28 16:28:30 +0530

    (auto-commit)

--- test.py
+++ test.py
@@ -442,7 +442,7 @@ def tracefunc(frame, event, arg, indent=[0]):
             print()
         print(" " * indent[0] + "> call function", frame.f_code.co_name, short_filename)
     elif event == "return":
-        print("<" + "-" * indent[0], "exit function", frame.f_code.co_name)
+        print("<" + " " * indent[0], "exit function", frame.f_code.co_name)
         indent[0] -= 2
     return tracefunc
 
