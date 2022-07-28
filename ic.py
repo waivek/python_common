27240f7e3a98e15c40d39ee90b39e989753e8931
Vivek Bose <waivek@gmail.com>
2022-07-28 19:07:15 +0530

    (auto-commit)

--- ic.py
+++ ic.py
@@ -519,7 +519,7 @@ def data_source_multiline_long_colored_variables():
 
     table = Table()
     local_D = locals()
-    local_D["local_D"] = "\n".join(textwrap.wrap(color_D_if_big(local_D), 40))
+    local_D["mystuff"] = "\n".join(textwrap.wrap(color_D_if_big(local_D), 40))
     for key, value in locals().items():
         if key == 'table':
             continue
