465f6c7f89d21e72a2f9232d1f12adcc50827725
Vivek Bose <waivek@gmail.com>
2022-07-28 19:15:30 +0530

    (auto-commit)

--- ic.py
+++ ic.py
@@ -519,9 +519,10 @@ def data_source_multiline_long_colored_variables():
 
     table = Table()
     local_D = locals()
-    local_D["mystuff"] = "\n".join(textwrap.wrap(color_D_if_big(local_D), 80))
+    mystuff = "\n".join(textwrap.wrap(color_D_if_big(local_D), 80))
     from rich import ansi
     decoder = ansi.AnsiDecoder()
+    print(decoder.decode(mystuff))
     # for key, value in locals().items():
     #     if key == 'table':
     #         continue
