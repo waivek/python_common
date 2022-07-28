ce2436fc1470c35e10a33b81e10f06a51fca22cc
Vivek Bose <waivek@gmail.com>
2022-07-28 16:44:00 +0530

    (auto-commit)

--- color.py
+++ color.py
@@ -133,5 +133,11 @@ def supports_color():
 
 if __name__ == "__main__":
     # from colorama import winterm, 
-    print_samples()
+
+    import os
+
+    print('\033[35m'+'color-test'+'\033[39m'+" test end")
+    os.system('') #enable VT100 Escape Sequence for WINDOWS 10 Ver. 1607
+    print ('\033[35m'+'color-test'+'\033[39m'+" test end")
+    # print_samples()
 
