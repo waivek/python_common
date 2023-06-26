4f0424c2dd01ceb76a06cde8c0468a3336cf867a
Vivek Bose <waivek@gmail.com>
2023-06-26 12:16:27 +0530

    (auto-commit)

--- setup.py
+++ setup.py
@@ -8,13 +8,13 @@ from setuptools import setup
 setup(
         name='common',
         version='1.0',
-        py_modules=['common', 'db', 'ic', 'error', 'timer', 'color'],
+        py_modules = ['color.py', 'common.py', 'db.py', 'error.py', 'get.py', 'ic.py', 'print_utils.py', 'reltools.py', 'timer.py'],
         install_requires = [
-            'aiohttp==3.8.1'
-            'executing==0.8.2'
-            'python_dateutil==2.8.2'
-            'timeago==1.0.14'
-            'pysqlite3-binary;Linux'
+            'aiohttp==3.8.1',
+            'executing==0.8.2',
+            'python_dateutil==2.8.2',
+            'timeago==1.0.14',
+            'pysqlite3-binary; platform_system=="Linux"'
         ]
 )
 
