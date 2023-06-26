d0eab04d0f094b25db4662a3ec8779a6fe912eb5
Vivek Bose <waivek@gmail.com>
2023-06-26 12:22:46 +0530

    (auto-commit)

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 setup(
         name='common',
         version='1.0',
-        py_modules = ['color.py', 'common.py', 'db.py', 'error.py', 'get.py', 'ic.py', 'print_utils.py', 'reltools.py', 'timer.py'],
+        py_modules = ['color', 'common', 'db', 'error', 'frame', 'get', 'ic', 'print_utils', 'reltools', 'timer', 'trace'],
         install_requires = [
             'aiohttp==3.8.1',
             'executing==0.8.2',
